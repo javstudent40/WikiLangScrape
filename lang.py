5554ffefa5b53d4fae7cc0fdd56fe15117ef1bcc
javstudent40 <titusgr30@yahoo.com>
2020-03-21 23:21:51 -0400

    added regex library returned some results from wiki table

--- lang.py
+++ lang.py
@@ -1,2 +1,22 @@
 import requests
 import pandas
+import re
+
+data = []
+
+url = "https://en.wikipedia.org/wiki/Comparison_of_programming_languages"
+
+response = requests.get(url)
+
+langlist = re.compile(r'<th><a href=\"/[\w\s\/]*\"[\w\s]*=\"[\w\s]*\">([\w\s]*)<\/a>[\w\s]*<\/th>[\w\s]*<td>([\w\s]*)<\/td>')
+
+languages = re.findall(langlist, response.text)
+
+for l in languages:
+	language = l[0]
+	description = l[1]
+	data.append((language, description))
+
+
+
+
